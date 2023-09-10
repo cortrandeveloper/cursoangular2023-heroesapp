02c7efc1c0eba7ba489e729117426449f6129d4d
javiercoronatecmx <112840715+javiercoronatecmx@users.noreply.github.com>
2023-09-10 12:53:44 -0700

    Rutas LazyLoad y Rutas hijas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -12,6 +13,7 @@ import { AppComponent } from './app.component';
     BrowserModule,
     AppRoutingModule
   ],
+  exports:[SharedModule],
   providers: [],
   bootstrap: [AppComponent]
 })
